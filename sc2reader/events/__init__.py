b7b556e419b5a869d96ffd41199d0c9cd0161f5e
BuRny <gamingburny@gmail.com>
2020-10-17 23:37:35 +0200

    Build without poetry environment

--- sc2reader/events/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import absolute_import, print_function, unicode_literals, division
-
-# Export all events of all types to the package interface
-from sc2reader.events import base, game, message, tracker
-from sc2reader.events.base import *
-from sc2reader.events.game import *
-from sc2reader.events.message import *
-from sc2reader.events.tracker import *
